fc720d55977c33fd033e8548b358a230e722f96d
Dehan <dehanseal@gmail.com>
2026-03-29 22:09:46 +0700

    add pydantic output agent

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from typing import List
+from pydantic import BaseModel, Field
+
 from dotenv import load_dotenv
 from langchain.agents import create_agent
 from langchain.tools import tool
@@ -7,12 +10,22 @@ from langchain_tavily import TavilySearch
 
 load_dotenv()
 
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
 
+    url:str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for agent response with answer and sources"""
+
+    answer:str = Field(description="The agent's answer to the query")
+    source: List[Source] = Field(default_factory=list, 
+                                 description="List of sources used to generate the answer")
 
 llm = ChatGoogleGenerativeAI(model="gemini-3.1-flash-lite-preview",
                              temperature=0)
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 def main():
     print("Hello from langchain-course!")
